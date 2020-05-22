499d06f7f9df4cf872ba122aec7df6bfd6352a02
I-TZU <family831113@gmail.com>
2020-05-22 16:10:45 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var flag = 1;
 if (flag === 1) {
   tabcontent = document.getElementsByClassName("tabcontent");
   tabcontent[0].style.display = "block";
+  flag = 0;
 }
 
 function openInfo(evt, infoName) {
@@ -26,22 +27,19 @@ function openInfo(evt, infoName) {
 }
 
 function showInfo(evt, infoName) {
-  // Declare all variables
+
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("info");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("imglinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(infoName).style.display = "block";
   evt.currentTarget.className += " active";
 }
